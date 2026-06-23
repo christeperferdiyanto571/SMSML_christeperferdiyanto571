20f0f9b24acd4e6fc769e947a9f006f39c96d147
christeperferdiyanto571 <121386122+christeperferdiyanto571@users.noreply.github.com>
2026-06-23 15:26:22 +0700

    Update 7.Inference.py

--- Monitoring dan Logging/7.Inference.py
+++ Monitoring dan Logging/7.Inference.py
@@ -1,1 +1,18 @@
 
+import requests
+
+# URL model (sesuaikan port jika berbeda)
+URL = "http://localhost:5001/invocations"
+
+def test():
+    data = {
+        "dataframe_split": {
+            "columns": ["MedInc", "HouseAge", "AveRooms", "AveBedrms", "Population", "AveOccup", "Latitude", "Longitude"],
+            "data": [[8.3, 41.0, 6.98, 1.02, 2228.0, 2.55, 37.88, -122.24]]
+        }
+    }
+    r = requests.post(URL, json=data)
+    print(r.json())
+
+if __name__ == "__main__":
+    test()
