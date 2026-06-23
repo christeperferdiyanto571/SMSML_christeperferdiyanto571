10c329b92f25f1d4a810cd4c7c289e1a5461f950
christeperferdiyanto571 <121386122+christeperferdiyanto571@users.noreply.github.com>
2026-06-23 15:25:23 +0700

    Update 3.prometheus_exporter.py

--- Monitoring dan Logging/3.prometheus_exporter.py
+++ Monitoring dan Logging/3.prometheus_exporter.py
@@ -1,1 +1,15 @@
 
+from prometheus_client import start_http_server, Gauge
+import time
+import random
+
+# Metrik untuk dimonitor
+MODEL_ACCURACY = Gauge('model_accuracy', 'Current Model Accuracy')
+
+if __name__ == '__main__':
+    start_http_server(8001)
+    print("Exporter jalan di port 8001")
+    while True:
+        # Simulasi nilai akurasi
+        MODEL_ACCURACY.set(random.uniform(0.8, 0.99))
+        time.sleep(5)
